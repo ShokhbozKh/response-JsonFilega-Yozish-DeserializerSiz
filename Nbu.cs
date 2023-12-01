08ddf78c2c372fca307e70748eed2bd100723964
Shohboz Xurramov <shohboztuit@gmail.com>
2023-12-01 21:19:33 +0500

    Add project files.

---
+++ Nbu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Nbu
+    {
+        public string title { get; set; }
+        public string code { get; set; }
+        public string cb_price { get; set; }
+        public string nbu_buy_price { get; set; }
+        public string nbu_cell_price { get; set; }
+        public string date { get; set; }
+
+        public override string ToString()
+        {
+            return $"";
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /// nbu ni jsonga yozaman
+            /// 7
+            /// 
+
+            string url = @"https://nbu.uz/uz/exchange-rates/json/";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetAsync(" ");
+            var deJson =  await response.Content.ReadAsStringAsync();
+
+            await Console.Out.WriteLineAsync(deJson);
+
+            var json = JsonSerializer.Deserialize<Nbu[]>(deJson);
+
+            foreach(var item in json)
+            {
+                Console.WriteLine(item.title);
+            }
+
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string fileName = $@"{path}\\file.json";
+
+            if(!File.Exists(fileName))
+            {
+                File.Create(fileName).Close();
+            }
+            using(StreamWriter aw  = new StreamWriter(fileName))
+            {
+                aw.WriteLine(deJson);
+            }
+        }
+    }
+}
